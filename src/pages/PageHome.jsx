e96a5f77fd14c085c67114034363d8e208851ca5
akbcit <kumaraditya.ca@gmail.com>
2024-01-15 21:32:28 -0800

    header scroll effect

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -3,18 +3,22 @@ import ArticleGrid from "../components/ArticleGrid";
 import FeaturedArticle from "../components/FeaturedArticle";
 import articles from "../data/articles.json";
 import selectFeaturedArticle from "../utils/selectFeaturedArticle";
+import HeaderNav from "../components/HeaderNav";
 
 function PageHome() {
   // select one featured article and get rest of the articles randomly
   const { featuredArticle, otherArticles } = selectFeaturedArticle(articles);
 
   return (
+    <>
+    <HeaderNav/>
     <main>
       <section id="articles-section">
         <FeaturedArticle article={featuredArticle} />
         <ArticleGrid articles={otherArticles} />
       </section>
     </main>
+    </>
   );
 }
 
