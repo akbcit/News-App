1a8f2e7bf129f3fef86ae4b05be21683c2249371
Knightmare12321 <qiu4135@gmail.com>
2024-01-17 09:50:47 -0800

    add sidebar laptops content

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,19 @@
 import "../styles/SideBar.css";
+import LaptopItem from "./LaptopItem"
 
-function SideBar() {
-  return <div className="side-bar"></div>;
+function SideBar({laptops}) {
+  return (
+    <div className="side-bar">
+      <div className="deal-today">
+        <h2>Today's Deal</h2> 
+      </div>
+      <div className="laptop-bar">
+        {laptops.map((laptop, index) => {
+          return(<LaptopItem key={`laptop-${index}`} laptop={laptop} laptopNum={index}/>)
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default SideBar;
