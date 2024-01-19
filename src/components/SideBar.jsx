034fadad1beefa84a9be4c94d1f2fa307cc9dab9
Knightmare12321 <qiu4135@gmail.com>
2024-01-18 16:23:41 -0800

    add sidebar animation and text hover effect

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,33 @@
 import "../styles/SideBar.css";
-import LaptopItem from "./LaptopItem"
+import LaptopItem from "./LaptopItem";
+import "animate.css";
 
-function SideBar({laptops}) {
+function SideBar({ laptops }) {
   return (
     <div className="side-bar">
       <div className="deal-today">
-        <h2>Today's Deal</h2> 
+        <h2
+          className="animate__animated
+                        animate__swing 
+                        animate__delay-1s
+                        animate__infinite"
+        >
+          Today's Deal
+        </h2>
       </div>
       <div className="laptop-bar">
         {laptops.map((laptop, index) => {
-          return(<LaptopItem key={`laptop-${index}`} laptop={laptop} laptopNum={index}/>)
+          return (
+            <LaptopItem
+              key={`laptop-${index}`}
+              laptop={laptop}
+              laptopNum={index}
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
 export default SideBar;
